2c148bcefc6903269a5048495b1fb4115873d306
greyspectrum <hello@greyspectrum.io>
2023-01-18 11:29:57 -0500

    nuke target again

--- twosum/target/debug/deps/twosum_trivial-99ef8d14e47d8424.d
+++
@@ -1,5 +0,0 @@
-/home/alsmith/rust/src/github.com/whiteboard/twosum/target/debug/deps/twosum_trivial-99ef8d14e47d8424: twosum-trivial/src/main.rs
-
-/home/alsmith/rust/src/github.com/whiteboard/twosum/target/debug/deps/twosum_trivial-99ef8d14e47d8424.d: twosum-trivial/src/main.rs
-
-twosum-trivial/src/main.rs:
--- twosum/target/debug/twosum-trivial.d
+++
@@ -1,1 +0,0 @@
-/home/alsmith/rust/src/github.com/whiteboard/twosum/target/debug/twosum-trivial: /home/alsmith/rust/src/github.com/whiteboard/twosum/twosum-trivial/src/main.rs
