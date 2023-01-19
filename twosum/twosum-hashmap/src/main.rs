88829a65f28019211ed18c29dbf8bd3e3acc41e7
greyspectrum <hello@greyspectrum.io>
2023-01-18 19:18:54 -0500

    twosum hashmap

--- twosum/twosum-hashmap/src/main.rs
+++ twosum/twosum-hashmap/src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+
+pub fn two_sum(nums: &[i32], target: i32) -> (usize, usize) {
+    let mut map = HashMap::new();
+
+    for (i, x) in nums.iter().enumerate() {
+        let df = target - x;
+        if map.contains_key(&df) {
+            return (*map.get(&df).unwrap(), i);
+        }
+        map.insert(x, i);
+    }
+    return (0, 0);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let ret = two_sum(&vec![2, 7, 11, 15], 9);
+    println!("{}, {}", ret.0, ret.1);
 }
