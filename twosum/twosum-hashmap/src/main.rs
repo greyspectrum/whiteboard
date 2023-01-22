5cee7a92e1c3bf66b6203888889b597aa136020d
greyspectrum <hello@greyspectrum.io>
2023-01-21 20:48:02 -0500

    remove comments

--- twosum/twosum-hashmap/src/main.rs
+++ twosum/twosum-hashmap/src/main.rs
@@ -1,27 +1,5 @@
 use std::collections::HashMap;
 
-//pub fn two_sum(nums: Vec<i32>, target: i32) -> (usize, usize) {
-//    let mut map = HashMap::new();
-//
-//    for (i, x) in nums.iter().enumerate() {
-//        let df = target - x;
-//        if map.contains_key(&df) {
-//            return (*map.get(&df).unwrap(), i);
-//        }
-//        map.insert(x, i);
-//    }
-//    return (0, 0);
-//}
-
-//fn main() {
-//    let nums = vec![4, 5, 6];
-//
-//    let target: i32 = 11;
-//
-//    let ret = two_sum(nums, target);
-//    println!("{}, {}", ret.0, ret.1);
-//}
-
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut map = HashMap::new();
 
