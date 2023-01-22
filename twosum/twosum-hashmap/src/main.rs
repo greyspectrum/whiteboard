79dfb61254e7e88b67f776be4ba1eeb2c50a4dbb
greyspectrum <hello@greyspectrum.io>
2023-01-21 20:46:21 -0500

    have fn return vec, not tuple

--- twosum/twosum-hashmap/src/main.rs
+++ twosum/twosum-hashmap/src/main.rs
@@ -1,23 +1,51 @@
 use std::collections::HashMap;
 
-pub fn two_sum(nums: Vec<i32>, target: i32) -> (usize, usize) {
+//pub fn two_sum(nums: Vec<i32>, target: i32) -> (usize, usize) {
+//    let mut map = HashMap::new();
+//
+//    for (i, x) in nums.iter().enumerate() {
+//        let df = target - x;
+//        if map.contains_key(&df) {
+//            return (*map.get(&df).unwrap(), i);
+//        }
+//        map.insert(x, i);
+//    }
+//    return (0, 0);
+//}
+
+//fn main() {
+//    let nums = vec![4, 5, 6];
+//
+//    let target: i32 = 11;
+//
+//    let ret = two_sum(nums, target);
+//    println!("{}, {}", ret.0, ret.1);
+//}
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut map = HashMap::new();
+
+    let mut solution: Vec<i32> = Vec::new();
 
     for (i, x) in nums.iter().enumerate() {
         let df = target - x;
         if map.contains_key(&df) {
-            return (*map.get(&df).unwrap(), i);
+            let sum_a = *map.get(&df).unwrap() as i32;
+            let sum_b = i as i32;
+            solution.push(sum_a);
+            solution.push(sum_b);
         }
         map.insert(x, i);
     }
-    return (0, 0);
+
+    return solution;
 }
 
 fn main() {
-    let nums = vec![2, 7, 11, 15];
+    let nums = vec![10, 5, 6];
 
-    let target: i32 = 9;
+    let target: i32 = 15;
 
     let ret = two_sum(nums, target);
-    println!("{}, {}", ret.0, ret.1);
+    println!("{:?}", ret);
 }
