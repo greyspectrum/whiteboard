62665db10255f68c6705a31ec6e703a678069822
greyspectrum <hello@greyspectrum.io>
2023-01-24 17:55:45 -0500

    add two

---
+++ add-two/target/debug/add-two.d
@@ -0,0 +1,1 @@
+/home/alsmith/rust/src/github.com/whiteboard/add-two/target/debug/add-two: /home/alsmith/rust/src/github.com/whiteboard/add-two/src/main.rs
---
+++ add-two/target/debug/deps/add_two-fd0b5bd1b990ce6b.d
@@ -0,0 +1,5 @@
+/home/alsmith/rust/src/github.com/whiteboard/add-two/target/debug/deps/add_two-fd0b5bd1b990ce6b: src/main.rs
+
+/home/alsmith/rust/src/github.com/whiteboard/add-two/target/debug/deps/add_two-fd0b5bd1b990ce6b.d: src/main.rs
+
+src/main.rs:
